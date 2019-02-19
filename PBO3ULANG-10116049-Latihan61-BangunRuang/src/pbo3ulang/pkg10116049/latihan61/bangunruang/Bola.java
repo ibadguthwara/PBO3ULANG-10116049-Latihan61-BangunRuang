9f84680d3410fdbf58eea09ced61def995d6a677
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-19 21:06:58 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan61-BangunRuang/src/pbo3ulang/pkg10116049/latihan61/bangunruang/Bola.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan61.bangunruang;
+
+/**
+ *
+ * @author ibadguthwara
+ */
+public class Bola extends BangunRuang {
+
+    @Override
+    public double volume() {
+        return 1.33333333333 * 3.142857142857143 * jari2 * jari2 * jari2 ;
+       
+    }
+
+   
+    
+
+    
+
+      
+    
+    
+    
+    
+}
